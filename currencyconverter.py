7a55b258b59e84280fc21e070631445b68da2995
AP <48294689+appandya84@users.noreply.github.com>
2021-04-04 15:26:15 +0100

    Update currencyconverter.py

--- currencyconverter.py
+++ currencyconverter.py
@@ -4,11 +4,6 @@ from ttkthemes import themed_tk as tk
 from tkinter import ttk
 import requests
 
-window = tk.ThemedTk()
-window.geometry('650x400')
-window.get_themes()
-window.set_theme('kroc')
-window.title('Currency Converter Calculator')
 
 def conversion():
 
@@ -34,37 +29,46 @@ def conversion():
     except ValueError:
         label7.config(text='The Amount Field is empty. Please try again with valid amount.')
 
-label1 = LabelFrame(window,text='FROM',pady=10,padx=10)
-label1.place(x=10,y=10)
 
-var1= StringVar()
-combo1=ttk.Combobox(label1,textvariable=var1,width=20)
-combo1['values'] = ('INR','USD','AUD','EUR','JPY','GBP','CAD','CHF')
-combo1.set('GBP')
-combo1.pack()
+if __name__ == '__main__':
+    
+    window = tk.ThemedTk()
+    window.geometry('650x400')
+    window.get_themes()
+    window.set_theme('kroc')
+    window.title('Currency Converter Calculator')
 
-label2 = LabelFrame(window,text='TO',pady=10,padx=10)
-label2.place(x=190,y=10)
+    label1 = LabelFrame(window,text='FROM',pady=10,padx=10)
+    label1.place(x=10,y=10)
 
-var2 = StringVar()
-combo2=ttk.Combobox(label2,textvariable=var2,width=20)
-combo2['values'] = ('INR','USD','AUD','EUR','JPY','GBP','CAD','CHF')
-combo2.set('EUR')
-combo2.pack()
+    var1= StringVar()
+    combo1=ttk.Combobox(label1,textvariable=var1,width=20)
+    combo1['values'] = ('INR','USD','AUD','EUR','JPY','GBP','CAD','CHF')
+    combo1.set('GBP')
+    combo1.pack()
 
-label3 = LabelFrame(window,text='AMOUNT',pady=10,padx=10)
-label3.place(x=370,y=10)
-entry1 = ttk.Entry(label3,width=20)
-entry1.pack()
-button1 = ttk.Button(text='CONVERT',command=conversion)
-button1.place(x=430,y=75)
-label4 = Label(text='')
-label4.place(x=16,y=90)
-label5 = Label(text='')
-label5.place(x=14,y=110)
-label6 = Label(text='')
-label6.place(x=14,y=180)
-label7 = Label(text='')
-label7.place(x=200,y=280)
+    label2 = LabelFrame(window,text='TO',pady=10,padx=10)
+    label2.place(x=190,y=10)
 
-window.mainloop()
+    var2 = StringVar()
+    combo2=ttk.Combobox(label2,textvariable=var2,width=20)
+    combo2['values'] = ('INR','USD','AUD','EUR','JPY','GBP','CAD','CHF')
+    combo2.set('EUR')
+    combo2.pack()
+
+    label3 = LabelFrame(window,text='AMOUNT',pady=10,padx=10)
+    label3.place(x=370,y=10)
+    entry1 = ttk.Entry(label3,width=20)
+    entry1.pack()
+    button1 = ttk.Button(text='CONVERT',command=conversion)
+    button1.place(x=430,y=75)
+    label4 = Label(text='')
+    label4.place(x=16,y=90)
+    label5 = Label(text='')
+    label5.place(x=14,y=110)
+    label6 = Label(text='')
+    label6.place(x=14,y=180)
+    label7 = Label(text='')
+    label7.place(x=200,y=280)
+
+    window.mainloop()
